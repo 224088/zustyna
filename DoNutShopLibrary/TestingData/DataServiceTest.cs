cf615499ef0827ada96758003fe3341d0f811345
DziobakSz <73827110+DziobakSz@users.noreply.github.com>
2020-11-16 22:31:15 +0100

    Forgot to add test class

---
+++ DoNutShopLibrary/TestingData/DataServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DataLayer;
+using DataLayer.DataGeneration;
+using LogicLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingData
+{
+	class DataServiceTests
+	{
+
+		private DataService service;
+		private DataContext our_shop;
+		private IGenerator generator;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			our_shop = new DataContext();
+			service = new DataService(new DataRepository(our_shop));
+			generator = new FixedGenerator();
+			generator.GenarateData(our_shop);
+		}
+
+		[TestMethod]
+		public void BuyDonutsTest()
+		{
+			Customer customer = new Customer("Wanda", "Owca", "W5");
+			service.AddCustomer(customer);
+			Donut donut = new Donut("Peanut butter&", 876, 8.77, DonutTypeEnum.Jelly);
+			DateTime now = DateTime.Now;
+			service.AddDonut(donut);
+			int stateThen = service.GetStateOfDonut(876);
+			service.BuyDonut("W5", 876, now, 17);
+			Assert.AreEqual(stateThen - 17, service.GetStateOfDonut(876));
+
+		}
+	}
+}
