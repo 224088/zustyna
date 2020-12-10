516c4fab6439a0730fcd9cbf3f2cfabde0e0b866
224088 <224088@edu.p.lodz.pl>
2020-12-10 14:43:41 +0100

    I think i worked that out!

--- Donuts2TheRevenge/DataTests/UnitTest1.cs
+++ Donuts2TheRevenge/DataTests/UnitTest1.cs
@@ -10,70 +10,65 @@ namespace DataTest
     [TestClass]
     public class DataTest
     {
-        static private string GetConnectionString()
-        {
-            return "Data Source=DESKTOP-56T1RJQ; AttachDbFilename =donut.mdf; Integrated Security = True";
-        }
+        
 
-        private static void OpenSqlConnection()
-        {
-            string connectionString = GetConnectionString();
-
-            using (SqlConnection connection = new SqlConnection())
-            {
-                connection.ConnectionString = connectionString;
-
-                connection.Open();
-
-                Console.WriteLine("State: {0}", connection.State);
-                Console.WriteLine("ConnectionString: {0}",
-                    connection.ConnectionString);
-            }
-        }
+        
 
         [TestMethod]
         public void AddDOnutToDatabase()
         {
             //OpenSqlConnection();
 
-            DataClasses1DataContext database = new DataClasses1DataContext();
+            using (DataClasses1DataContext database = new DataClasses1DataContext("Data Source=DESKTOP-H5C7HVQ;Initial Catalog=donut;Integrated Security=True"))
+            {
 
-            donut donut = new donut();
-            donut.donut_id = 2;
-            donut.donut_name = "FilledWithJoy";
-            donut.fillling = "Joy";
-            donut.price = 8;
-            
-
-
-
-            database.donut.InsertOnSubmit(donut);
-            database.SubmitChanges();
+                donut donut = new donut();
+                donut.donut_id = 2;
+                donut.donut_name = "FilledWithJoy";
+                donut.fillling = "Joy";
+                donut.price = 8;
 
 
 
-            donut mydonut = database.donut.FirstOrDefault(doonut => doonut.donut_id.Equals(2));
-            Assert.AreEqual(mydonut.donut_id, 2);
-            Assert.AreEqual(mydonut.donut_name, "FilledWithJoy");
-            Assert.AreEqual(mydonut.fillling, "Joy");
-            Assert.AreEqual(mydonut.price, 8);
+
+                database.donut.InsertOnSubmit(donut);
+                database.SubmitChanges();
 
 
 
-            database.donut.DeleteOnSubmit(donut);
-            database.SubmitChanges();
+                donut mydonut = database.donut.FirstOrDefault(doonut => doonut.donut_id.Equals(2));
+                Assert.AreEqual(mydonut.donut_id, 2);
+                Assert.AreEqual(mydonut.donut_name, "FilledWithJoy");
+                Assert.AreEqual(mydonut.fillling, "Joy");
+                Assert.AreEqual(mydonut.price, 8);
+
+
+
+                database.donut.DeleteOnSubmit(donut);
+                database.SubmitChanges();
+            }
         }
 
-     /*   [TestMethod]
-        public void fetchFromDBTest()
-        {
-            LibraryLinqDataContext db = new LibraryLinqDataContext();
+           [TestMethod]
+             [ExpectedException(typeof(System.Data.SqlClient.SqlException))]
+           public void ConnectingToNonExsistingDB()
+           {
+            using (DataClasses1DataContext fake = new DataClasses1DataContext("Data Source=DESKTOP-H5C7HVQ;Initial Catalog=WrongNAme;Integrated Security=True"))
+            {
 
-            Customers customer = db.Customers.FirstOrDefault(c => c.id.Equals(100));
-            Assert.AreEqual(customer.id, 100);
-            Assert.AreEqual(customer.name, "John");
-            Assert.AreEqual(customer.money, 100);
+                donut donut = new donut();
+                donut.donut_id = 2;
+                donut.donut_name = "FilledWithJoy";
+                donut.fillling = "Joy";
+                donut.price = 8;
 
-        }*/
+
+
+
+                fake.donut.InsertOnSubmit(donut);
+                fake.SubmitChanges();
+            }
+
+           }
     }
 }
