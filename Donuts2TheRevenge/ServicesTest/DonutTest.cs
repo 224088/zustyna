0052126c09e0f57ffe8b6338e1f8561994f7605a
DziobakSz <73827110+DziobakSz@users.noreply.github.com>
2020-12-10 22:24:35 +0100

    Did CUD and kinda R for donut (need to add methods for retriving by fiiling) Created test project for services

---
+++ Donuts2TheRevenge/ServicesTest/DonutTest.cs
@@ -0,0 +1,18 @@
+﻿using Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services;
+using System;
+
+namespace ServicesTest
+{
+    [TestClass]
+    public class DonutTest
+    {
+        [TestMethod]
+        public void AddDonutTest()
+        {
+
+         //   Assert.IsTrue(donutCRUD.addDonut(666, 2, "Dewvil Wears Prada", "Fashiom", 800));
+        }
+    }
+}
