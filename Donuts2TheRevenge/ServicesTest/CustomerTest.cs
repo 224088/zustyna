42f34cb9cf50b1b9f7d23b9c6c392d334a958569
DziobakSz <73827110+DziobakSz@users.noreply.github.com>
2020-12-14 00:10:22 +0100

    Lacks tests for IEnumerables and for time (idk how to do it) and type for Event

---
+++ Donuts2TheRevenge/ServicesTest/CustomerTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services;
+using System;
+
+namespace ServicesTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void AddCustomerTest()
+        {
+
+            Assert.IsTrue(CustomerCRUD.addCustomer(67, "Michael", "Jackson"));
+            Assert.IsTrue(CustomerCRUD.deleteCustomer(67));
+        }
+        [TestMethod]
+        public void GetCustomerTest()
+        {
+            CustomerCRUD.addCustomer(88, "Joan", "Jett");
+            Assert.AreEqual(CustomerCRUD.GetCustomer(88).customer_f_name, "Joan");
+            Assert.AreEqual(CustomerCRUD.GetCustomerByLastName("Jett").customer_id, (88));
+            CustomerCRUD.deleteCustomer(88);
+
+        }
+
+        [TestMethod]
+        public void UpdateCustomerTest()
+        {
+            CustomerCRUD.addCustomer(77, "Ellen", "DeGeneres");
+            Assert.IsTrue(CustomerCRUD.updateLastName(77, "Page"));
+            Assert.AreEqual(CustomerCRUD.GetCustomer(77).customer_l_name, "Page");
+            Assert.IsTrue(CustomerCRUD.updateName(77, "Elliot"));
+            Assert.AreEqual(CustomerCRUD.GetCustomerByLastName("Page").customer_f_name, "Elliot");
+            CustomerCRUD.deleteCustomer(77);
+        }
+    }
+}
---
+++ Donuts2TheRevenge/ServicesTest/EventTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services;
+using System;
+
+namespace ServicesTest
+{
+    [TestClass]
+    public class EventTest
+    {
+        [TestMethod]
+        public void AddEventTest()
+        {
+
+            Assert.IsTrue(EventCRUD.addEvent(9, DateTime.Now, true, 6, 7, 5));
+            Assert.IsTrue(EventCRUD.deleteEvent(9));
+        }
+        [TestMethod]
+        public void GetEventTest()
+        { 
+            EventCRUD.addEvent(17, DateTime.Now, false, 72, 4,5 );
+            Assert.AreEqual(EventCRUD.GetEvent(17).amount, 72);
+            //Assert.AreEqual(EventCRUD.GetEventByTime(DateTime.????).event_id, 17);
+            EventCRUD.deleteEvent(17);
+
+        }
+
+        [TestMethod]
+        public void UpdateEventTest()
+        {
+            EventCRUD.addEvent(68, DateTime.Now, true, 55, 4, 5);
+            Assert.IsTrue(EventCRUD.updateAmount(68, 76));
+            Assert.AreEqual(EventCRUD.GetEvent(68).amount, 76);
+            Assert.IsTrue(EventCRUD.updateDonut(68, 7));
+            Assert.AreEqual(EventCRUD.GetEvent(68).donut, 7);
+            Assert.IsTrue(EventCRUD.updateCustomer(68, 6));
+            Assert.AreEqual(EventCRUD.GetEvent(68).customer, 6);
+            EventCRUD.deleteEvent(68);
+            
+            //brakuje type i czas
+        }
+    }
+}
